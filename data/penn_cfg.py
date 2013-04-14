2e6e11b16dcf1976523237441830857777bfa097
Peter Snyder <snyderp@gmail.com>
2013-04-14 05:45:47 -0500

    Remove unused files

--- data/penn_cfg.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.corpus import treebank
-from nltk.grammar import ContextFreeGrammar, Nonterminal
-
-tbank_productions = set(production for sent in treebank.parsed_sents()
-                        for production in sent.productions())
-tbank_grammar = ContextFreeGrammar(Nonterminal('S'), list(tbank_productions))
-
-print tbank_grammar.is_lexical()
