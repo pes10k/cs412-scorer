c8bb4d9cdbecbb3e2239abc394d436e8bdac9421
Peter Snyder <snyderp@gmail.com>
2013-04-15 16:34:03 -0500

    HMM / Parse tree based sentence tokenizer

---
+++ sentence_tokenizer.py
@@ -0,0 +1,52 @@
+import stanford_parser
+import tree_utils
+import hmm_utils
+import essay_utils
+
+counts = hmm_utils.get_transition_counts()
+
+
+def _possible_sentences_in_line(line, min_sentence_len=3):
+    sentences = [[line]]
+    words = line.replace(".", "").split()
+    num_words = len(words)
+    if num_words >= 6:
+        sentences += [[" ".join(words[0:i]), " ".join(words[i:])] for i in range(min_sentence_len, num_words - min_sentence_len)]
+    return sentences
+
+
+def prod(nums):
+    total = 1
+    for num in nums:
+        total *= num
+    return num
+
+
+def avg_prob(nums):
+    return sum(nums)/len(nums)
+
+
+for essay in essay_utils.essays[1:2]:
+    for line in essay[5:6]:
+        print "Working on: %s" % (line,)
+        all_possible_sentences = _possible_sentences_in_line(line)
+        all_possible_sentence_probs = []
+        for possible_sentences in all_possible_sentences:
+            print possible_sentences
+            prob_for_sentences = []
+            for possible_sentence in possible_sentences:
+                sentence_tree = stanford_parser.parse(possible_sentence)[0]
+                sentence_transitions = tree_utils.transitions_in_tree(sentence_tree)
+                sentence_probs = []
+                for transition in sentence_transitions:
+                    probs = hmm_utils.prob_of_all_transitions(transition, counts, gram_size=3)
+                    sentence_probs += probs
+                prob_for_sentences.append(prod(sentence_probs))
+            print avg_prob(prob_for_sentences)
+            all_possible_sentence_probs.append(avg_prob(prob_for_sentences))
+        max_prob = max(all_possible_sentence_probs)
+        parse_for_max_prob = all_possible_sentences[all_possible_sentence_probs.index(max_prob)]
+        print " -- MAX Prob: %f" % (max_prob,)
+        print " -- Parse for max prob: %s" % (parse_for_max_prob,)
+        print " -- Num Sentences: %d" % (len(parse_for_max_prob),)
+        print " -------------"
