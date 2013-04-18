e6805ce06b81f55b262122a14ed27c34b8d10d94
Peter Snyder <snyderp@gmail.com>
2013-04-18 16:31:13 -0500

    Improvements to subject-verb agreement code

--- agreement_utils.py
+++ agreement_utils.py
@@ -11,6 +11,7 @@ noun_tags = singluar_noun_tags + plural_noun_tags + ('PRP',)
 
 plural_verb_tags = ('VBZ',)
 singular_verb_tags = ('VBP',)
+verb_tags = singular_verb_tags + plural_verb_tags
 
 plural_prop_nouns = ('they', 'we', 'them', 'themselves', 'us', 'those')
 singluar_prop_nouns = ('he', 'she', 'i', 'him', 'me', 'myself', 'it')
@@ -38,17 +39,47 @@ def check_node_agreement(tree_one, tree_two):
         # Something wrong happened
         raise Exception("No noun tree in this agreement pair!")
 
-    if verb_tree.node in singular_verb_tags:
+    commanding_verb_tree = find_commanding_verb_tree(verb_tree)
+
+    if commanding_verb_tree.node in singular_verb_tags:
         verb_singular = True
-    elif verb_tree.node in plural_verb_tags:
+    elif commanding_verb_tree.node in plural_verb_tags:
         verb_singular = False
     else:
         raise Exception("No verb in this agrement pair!")
 
-    log("Noun: Looks like %s is %s (%s)" % (noun_tree[0], 'Singular' if noun_singular else 'Plural', "3rd" if noun_3rd_person else "1st"), 1)
-    log("Verb: Looks like %s is %s" % (verb_tree[0], 'Singular' if verb_singular else 'Plural'), 1)
+    log("Noun: Looks like %s is %s (%s)" % (noun_tree[0], 'Singular' if noun_singular else 'Plural', "3rd" if noun_3rd_person else "1st"), 2)
+    log("Verb: Looks like %s is %s" % (commanding_verb_tree[0], 'Singular' if verb_singular else 'Plural'), 2)
 
-    return noun_singular != verb_singular
+    noun_1st_person = not noun_3rd_person
+    is_vbp = commanding_verb_tree.node == "VBP"
+    is_vbz = commanding_verb_tree.node == "VBZ"
+
+    if noun_singular and noun_1st_person and is_vbp:
+        return True
+    elif noun_singular and noun_3rd_person and is_vbz:
+        return True
+    elif not noun_singular and noun_3rd_person and is_vbp:
+        return True
+    else:
+        log("DONT LIKE COMBO: %s" % ({"verb_tag": commanding_verb_tree.node, "noun_1st_person": noun_1st_person, "noun_singular": noun_singular},), 2)
+        return False
+
+
+def find_commanding_verb_tree(tree):
+    if tree.node in verb_tags:
+        return tree
+    else:
+        parent_node = tree.parent()
+        if not parent_node:
+            return None
+        else:
+            for sibling in parent_node:
+                if sibling.node in verb_tags:
+                    return sibling
+                elif sibling.node == "VP":
+                    return list(sibling.subtrees(lambda x: x.node in verb_tags))[0]
+            return find_commanding_verb_tree(parent_node)
 
 
 def is_pronoun_first_person(tree):
@@ -88,14 +119,17 @@ def parse(text, use_cache=False):
             dependencies = stanford_parser.dependences(sentence)
             sub_verb_deps = [dep for dep in dependencies if dep['dep_name'] == 'nsubj']
 
+            if len(sub_verb_deps) == 0:
+                log("Couldn't find Subject-Verb dependency info", 1)
+                continue
+
             for sub_verb in sub_verb_deps:
                 first_node = node_in_tree(tree, sub_verb['first_word'])
                 sec_node = node_in_tree(tree, sub_verb['second_word'])
                 if first_node and sec_node:
 
-                    print tree
-                    log("First Dep Node: %s" % (first_node,), 1)
-                    log("Sec Dep Node: %s" % (sec_node,), 1)
+                    log("First Dep Node: %s" % (first_node,), 2)
+                    log("Sec Dep Node: %s" % (sec_node,), 2)
 
                     try:
                         is_agreement = check_node_agreement(first_node, sec_node)
